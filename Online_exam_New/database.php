5a44880073528f7977815d091fbac3c7b0a99049
2015pgcaca44 <35236887+2015pgcaca44@users.noreply.github.com>
2018-01-15 14:12:26 +0530

    Add files via upload

---
+++ Online_exam_New/database.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(1);
+$cn=mysql_connect("localhost","root","") or die("Could not Connect My Sql");
+mysql_select_db("    C:\xampp\htdocs\Online_exam_New\Online_exam_New\database\quiz_new.sql.zip: Cannot create C:\xampp\htdocs\Online_exam_New\Online_exam_New\database\quiz_new.sql
+!   Access is denied.",$cn)  or die("Could connect to Database");
+?>
+online_new_exam
